afe1c6adc6a5bce8c04e36fae9e23aa60c9fa081
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2024-06-06 01:57:01 +0200

    Add Stack Overflow Logo (#47)

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,7 +1,8 @@
 import {View} from 'react-native';
 import linkedin from '../images/logo-linkedin.png';
 import github from '../images/logo-github-white.svg';
-import stackoverflow from '../images/stackoverflow.svg';
+import stackoverflow from '../images/logo-stackoverflow-white.svg';
+import hoverStackoverflow from '../images/logo-stackoverflow-black.svg';
 import codingame from '../images/logo-codingame-white.png';
 import hoverCodingame from '../images/logo-codingame-black.png';
 import hackerrank from '../images/hackerrank.svg';
@@ -10,7 +11,7 @@ import HorizontalLinkButton from './buttons/HorizontalLinkButton';
 const socialLinks = [
     { image: linkedin, altText: "Link to my LinkedIn-page.", text: "LinkedIn", url: "https://linkedin.com/in/chrimle"},
     { image: github, altText: "Link to my GitHub-page.", text: "GitHub", url: "https://github.com/chrimle"},
-    { image: stackoverflow, altText: "Link to my StackOverflow-page.", text: "StackOverflow", url: "https://stackoverflow.com/users/5424535/chrimle"},
+  { image: stackoverflow, hoverImage: hoverStackoverflow, altText: "My Profile on Stack Overflow", url: "https://stackoverflow.com/users/5424535/chrimle" },
   { image: codingame, hoverImage: hoverCodingame, altText: "Link to my Codingame-page.", url: "https://www.codingame.com/profile/a77d3f05e778f45ddc96b8c5693bdf913730442" },
     { image: hackerrank, altText: "Link to my HackerRank-page.", text: "HackerRank", url: "https://www.hackerrank.com/Chrimle"},
   ];
