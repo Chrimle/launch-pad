b1d6a91bde4cb2cdd5e6e5b252509f908911c7d0
Chrimle <krillestoffer1998@gmail.com>
2024-06-07 22:23:47 +0200

    style: redesign social media links header

--- src/components/containers/domains/social-media/SocialMediaLinks.js
+++ src/components/containers/domains/social-media/SocialMediaLinks.js
@@ -1,18 +1,14 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 import TextLinkButton from '../../../buttons/TextLinkButton';
 import theme from '../../../../theme';
 import SocialMedia from './SocialMedia';
+import styles from './styles/style-SocialMediaLinks';
 
 export default function SocialMediaLinks() {
 
-  return <View style={{ marginTop: '50px' }}>
-    <text style={{ fontStyle: 'italic' }}>
-      <text>Other</text>
-      <text style={{ color: theme.PRIMARY_COLOR }}> Verified </text>
-      <text>Socials</text>
-    </text>
-
-    <View style={{ flexDirection: 'row' }}>
+  return <View style={styles.container}>
+    <Text style={styles.header}>Other <text style={{ color: theme.PRIMARY_COLOR }}>Verified</text> Socials</Text>
+    <View style={styles.buttonsContainer}>
       {SocialMedia().map(socialMediaLink =>
         <TextLinkButton text={socialMediaLink.text} url={socialMediaLink.url} hoverText={socialMediaLink.hoverText} />
       )}
---
+++ src/components/containers/domains/social-media/styles/style-SocialMediaLinks.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+import theme from "../../../../../theme";
+
+
+const styles = StyleSheet.create({
+    container: {
+        margin: '25px',
+    },
+    header: {
+        color: 'white',
+        fontSize: '32pt',
+        textAlign: 'center',
+        borderBottomColor: theme.PRIMARY_COLOR,
+        borderBottomWidth: '5px'
+    },
+    buttonsContainer: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '25px',
+        width: 'auto',
+    },
+});
+
+export default styles;
