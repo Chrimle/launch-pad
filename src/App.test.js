b50e3b70902208d91e230583768fbe22a06ca4b5
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2024-06-10 22:03:00 +0200

    Add Tests for `App.js` (#61)

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,73 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 
 import Title from './components/Title';
+import Description from './components/Description';
+import SubTitle from './components/SubTitle';
+import SocialLinks from './components/containers/domains/socials/SocialLinks';
+import SocialMediaLinks from './components/containers/domains/social-media/SocialMediaLinks';
+import Footer from './components/Footer';
 
 test('renders chrimle title', () => {
   render(<Title />);
-  const linkElement = screen.getByText(/Chrimle/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/Chrimle/i)).toBeInTheDocument();
 });
+
+test('renders sub title', () => {
+  render(<SubTitle />);
+  expect(screen.getByText(/Software Engineer/)).toBeInTheDocument();
+});
+
+test('renders description', () => {
+  render(<Description />);
+  expect(screen.getByText(/Welcome/)).toBeInTheDocument();
+});
+
+test('renders social links', () => {
+  render(<SocialLinks />);
+  expect(screen.getByText(/My Socials/)).toBeInTheDocument();
+  const socials = screen.getAllByRole('link');
+  socials.forEach(social => {
+    expect(social).toBeInTheDocument();
+    // Check Link
+    expect(social).toHaveAttribute('href');
+    expect(social.getAttribute('href')).toMatch(/^https.*$/);
+    // Check Image
+    const image = within(social).getByRole('img');
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src');
+    expect(image.getAttribute('src')).toMatch(/^logo-.*$/);
+    // Check Tooltip
+    expect(social).toHaveAttribute('title');
+    expect(social.getAttribute('title')).toMatch(/^My Profile on .*$/);
+  });
+  expect(socials.length).toBeGreaterThan(0);
+});
+
+test('renders social media links', () => {
+  render(<SocialMediaLinks />);
+  expect(screen.getByText(/Other/)).toBeInTheDocument();
+  expect(screen.getByText(/Verified/)).toBeInTheDocument();
+  expect(screen.getByText(/Socials/)).toBeInTheDocument();
+  const socialMedias = screen.getAllByRole('link');
+  socialMedias.forEach(socialMedia => {
+    expect(socialMedia).toBeInTheDocument();
+    // Check Link
+    expect(socialMedia).toHaveAttribute('href');
+    expect(socialMedia.getAttribute('href')).toMatch(/^https.*$/);
+    // Check Text
+    expect(socialMedia.textContent).toBeTruthy();
+    // Check Tooltip
+    expect(socialMedia).toHaveAttribute('title');
+    expect(socialMedia.getAttribute('title')).toMatch(/^My Profile on .*$/);
+  });
+  expect(socialMedias.length).toBeGreaterThan(0);
+});
+
+test('renders footer', () => {
+  render(<Footer />);
+  expect(screen.getByText(/Chrimle/)).toBeInTheDocument();
+  const github = screen.getByText(/GitHub/);
+  expect(github).toBeInTheDocument();
+  expect(github).toHaveAttribute('href');
+  expect(github.getAttribute('href')).toMatch(/^https.*$/);
+})
