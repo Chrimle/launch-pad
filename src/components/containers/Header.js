9fed9b22e5e9d94f338cd00249602fe04d498942
Chrimle <28791817+Chrimle@users.noreply.github.com>
2024-06-20 21:15:04 +0200

    refactor: add Header-styling file

--- src/components/containers/Header.js
+++ src/components/containers/Header.js
@@ -1,10 +1,11 @@
 import { View } from "react-native-web";
 import Title from "./domains/headers/Title";
 import SubTitle from "./domains/headers/SubTitle";
+import styles from "./Header.styles";
 
 
 export default function Header() {
-    return <View>
+    return <View style={styles.container}>
         <Title />
         <SubTitle />
     </View>
