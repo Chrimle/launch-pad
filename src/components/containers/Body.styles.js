d0b9b609de439e951438c7f5a22b8e1a6ccf5d33
Chrimle <28791817+Chrimle@users.noreply.github.com>
2024-06-21 20:23:37 +0200

    refactor: move body-styling from css to stylesheet

---
+++ src/components/containers/Body.styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    }
+});
+
+export default styles;
