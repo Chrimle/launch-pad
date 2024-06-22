8e99f81b9c5f569613d71c5fdd09bce8ecbab848
Chrimle <28791817+Chrimle@users.noreply.github.com>
2024-06-22 21:30:31 +0200

    refactor: migrate NotFound-styling to new theme

--- src/components/containers/NotFound.styles.js
+++ src/components/containers/NotFound.styles.js
@@ -8,21 +8,21 @@ const styles = StyleSheet.create({
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'flex-start',
-        backgroundColor: theme.BACKGROUND_COLOR,
+        backgroundColor: theme.Colors.DOMINANT,
         minHeight: '100vh',
         paddingVertical: '2em',
     },
     text: {
-        color: 'white',
-        fontSize: '2em',
-        borderBottomColor: theme.PRIMARY_COLOR,
+        color: theme.Colors.COMPLEMENT,
+        fontSize: theme.Fonts.Sizes.HEADER,
+        borderBottomColor: theme.Colors.ACCENT,
         borderBottomWidth: '0.2em',
         marginBottom: '0.5em',
     },
     descriptionText: {
-        color: 'white',
+        color: theme.Colors.COMPLEMENT,
         fontStyle: 'italic',
-        fontSize: '1em'
+        fontSize: theme.Fonts.Sizes.PARAGRAPH,
     }
 });
 
