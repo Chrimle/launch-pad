4cb8a04c6593362fa646b0ab6331e84912b119dd
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2024-06-22 17:55:52 +0200

    Redirect to Home-page from 'Not Found'-page after 5 seconds (#86)

--- src/components/containers/NotFound.js
+++ src/components/containers/NotFound.js
@@ -1,9 +1,22 @@
 import { View, Text } from "react-native-web";
 import styles from "./NotFound.styles.js";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function NotFound() {
+    const [seconds, setSeconds] = useState(5);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        setInterval(() => setSeconds(seconds - 1), 1000)
+        if (seconds <= 0) {
+            navigate("/");
+        }
+    })
+
     return <View style={styles.container}>
         <Text style={styles.text}>Page Not Found</Text>
         <Text style={styles.descriptionText}>Not quite sure how You got here...</Text>
+        <Text style={styles.descriptionText}>Redirecting You to the Home-page in {seconds} seconds...</Text>
     </View>
 }
