4212d659c71c42b06a48a675faf04c0f22adc24c
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2024-06-22 15:20:52 +0200

    Add Routing-support (#82)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Footer from './components/containers/Footer';
 import Header from './components/containers/Header';
 import Body from './components/containers/Body';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         <Header />
       </header>
       <body>
-        <Body />
+        <Routes>
+          <Route path='*' element={<Body />} />
+        </Routes>
       </body>
       <footer>
         <Footer />
