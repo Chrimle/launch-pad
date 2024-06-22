c8d822835a186054b2f9fc524425ab6b667e9830
Chrimle <28791817+Chrimle@users.noreply.github.com>
2024-06-22 17:14:13 +0200

    feature: redirect to home-page from 'Not Found'-page after 5 seconds

--- src/components/containers/__tests__/NotFound.test.js
+++ src/components/containers/__tests__/NotFound.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import NotFound from "../NotFound";
+import { RouterProvider, createMemoryRouter } from 'react-router-dom';
 
 test('renders the NotFound-component', () => {
-    render(<NotFound />);
+    const router = createMemoryRouter([{ path: '*', element: <NotFound /> }])
+    render(<RouterProvider router={router} />);
     expect(screen.getByText(/Page Not Found/)).toBeInTheDocument();
     expect(screen.getByText(/Not quite sure how You got here.../)).toBeInTheDocument();
+    expect(screen.getByText(/Redirecting You to the Home-page in \d seconds.../)).toBeInTheDocument();
 });
